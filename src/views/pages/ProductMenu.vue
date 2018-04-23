45aa5f7e769112fefbc59a2d0f5a3cacadb166be
Dominic Lee <leemflee@gmaill.com>
2018-04-23 18:42:15 +0800

    testing flex limit height

--- src/views/pages/ProductMenu.vue
+++ src/views/pages/ProductMenu.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="d-flex flex-grow-1 flex-row align-items-stretch justify-content-around animated fadeIn"
          style="height:100%;">
-        <div class="d-flex p-2 flex-column flex-grow-0 bg-default" style="max-width:400px;">
+        <div class="d-flex p-2 flex-column flex-grow-0 bg-default" style="width:400px;">
             <div class="flex-grow-0 align-items-center justify-content-around mb-2">
                 <button class="pb-0 btn btn-primary"><i class="icon-film icons d-block"></i>
                     Order</button>
@@ -10,13 +10,13 @@
                 <button class="pb-0 btn btn-primary"><i class="icon-film icons d-block"></i>
                     Order</button>
             </div>
-            <div class="flex-grow-0 align-items-end bg-dark text-white justify-content-around mb-2">
+            <div class="flex-grow-0 bg-dark text-white justify-content-around mb-2">
                 <h4 class="text-center py-1">Order #234232</h4>
             </div>
-            <div class="flex-grow-1 text-body bg-info justify-content-around" style="padding-right:10px;overflow-y:scroll">
-                <div style="overflow-y:scroll;height:100%;">
-                    <ul class="list-group">
-                        <li class="d-flex flex-row list-group-item p-1">
+            <div class="d-flex flex-grow-1 text-body bg-info" style="padding-right:0px;position:relative;">
+                <div class="d-flex" style="overflow-y:scroll;padding-right:10px;width:100%;height:100%;position:absolute;">
+                    <ul class="list-group" style="position:relative;">
+                        <li class="d-flex flex-row list-group-item p-1" v-for="i in (1,20)" style="height:100px;">
                             <div class="row">
                                 <div class="d-flex flex-column pl-3 col-4 justify-content-center">
                                     <h5 class="m-0">叉雞飯</h5>
@@ -40,122 +40,19 @@
                                     <input class="text-right form-control-plaintext" readonly value="100.00"/>
                                 </div>
                             </div>
+                            <div style="clear:both;"></div>
                         </li>
-                        <li class="d-flex flex-row list-group-item p-1">
-                            <div class="row">
-                                <div class="d-flex flex-column pl-3 col-4 justify-content-center">
-                                    <h5 class="m-0">叉雞飯</h5>
-                                </div>
-                                <div class="col-5">
-                                    <div class="flex-grow-1">
-                                        <div class="form-group m-1">
-                                            <div class="input-group">
-                                                <button class="btn btn-sm">
-                                                    <i class="fa fa-fw fa-plus d-block"></i>
-                                                </button>
-                                                <input class="text-center form-control" value="2"/>
-                                                <button class="btn btn-sm">
-                                                    <i class="fa fa-fw fa-minus d-block"></i>
-                                                </button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="d-flex flex-column justify-content-center col-3">
-                                    <input class="text-right form-control-plaintext" readonly value="100.00"/>
-                                </div>
-                            </div>
-                        </li>
-                        <li class="d-flex flex-row list-group-item p-1">
-                            <div class="row">
-                                <div class="d-flex flex-column pl-3 col-4 justify-content-center">
-                                    <h5 class="m-0">叉雞飯</h5>
-                                </div>
-                                <div class="col-5">
-                                    <div class="flex-grow-1">
-                                        <div class="form-group m-1">
-                                            <div class="input-group">
-                                                <button class="btn btn-sm">
-                                                    <i class="fa fa-fw fa-plus d-block"></i>
-                                                </button>
-                                                <input class="text-center form-control" value="2"/>
-                                                <button class="btn btn-sm">
-                                                    <i class="fa fa-fw fa-minus d-block"></i>
-                                                </button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="d-flex flex-column justify-content-center col-3">
-                                    <input class="text-right form-control-plaintext" readonly value="100.00"/>
-                                </div>
-                            </div>
-                        </li>
-                        <li class="d-flex flex-row list-group-item p-1">
-                            <div class="row">
-                                <div class="d-flex flex-column pl-3 col-4 justify-content-center">
-                                    <h5 class="m-0">叉雞飯</h5>
-                                </div>
-                                <div class="col-5">
-                                    <div class="flex-grow-1">
-                                        <div class="form-group m-1">
-                                            <div class="input-group">
-                                                <button class="btn btn-sm">
-                                                    <i class="fa fa-fw fa-plus d-block"></i>
-                                                </button>
-                                                <input class="text-center form-control" value="2"/>
-                                                <button class="btn btn-sm">
-                                                    <i class="fa fa-fw fa-minus d-block"></i>
-                                                </button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="d-flex flex-column justify-content-center col-3">
-                                    <input class="text-right form-control-plaintext" readonly value="100.00"/>
-                                </div>
-                            </div>
-                        </li>
-                        <li class="d-flex flex-row list-group-item p-1">
-                            <div class="row">
-                                <div class="d-flex flex-column pl-3 col-4 justify-content-center">
-                                    <h5 class="m-0">叉雞飯</h5>
-                                </div>
-                                <div class="col-5">
-                                    <div class="flex-grow-1">
-                                        <div class="form-group m-1">
-                                            <div class="input-group">
-                                                <button class="btn btn-sm">
-                                                    <i class="fa fa-fw fa-plus d-block"></i>
-                                                </button>
-                                                <input class="text-center form-control" value="2"/>
-                                                <button class="btn btn-sm">
-                                                    <i class="fa fa-fw fa-minus d-block"></i>
-                                                </button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="d-flex flex-column justify-content-center col-3">
-                                    <input class="text-right form-control-plaintext" readonly value="100.00"/>
-                                </div>
-                            </div>
-                        </li>
-                        <li class="list-group-item">叉雞飯</li>
-                        <li class="list-group-item">叉雞飯</li>
-                        <li class="list-group-item">叉雞飯</li>
-                        <li class="list-group-item">叉雞飯</li>
-                        <li class="list-group-item">叉雞飯</li>
-                        <li class="list-group-item">叉雞飯</li>
                    </ul>
                 </div>
             </div>
-
+            <div class="flex-grow-0 bg-dark text-white">
+                <h4 class="text-center py-1">Order #234232</h4>
+            </div>
         </div>
-        <div class="p-2 align-items-stretch flex-column flex-grow-1 bg-secondary">
+        <div class="p-2 align-items-stretch flex-column flex-grow-1 bg-light">
             <h4>Column 1</h4>
         </div>
-        <div class="p-2 align-items-stretch flex-column flex-grow-1 bg-success">
+        <div class="p-2 align-items-stretch flex-column flex-grow-1 bg-light">
             <h4>Column 1</h4>
         </div>
     </div>
